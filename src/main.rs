796ad021e90a12b48735feec15baacb621180179
ArcticPheenix <phoenix.ckelly@gmail.com>
2019-06-23 22:12:42 -0600

    Added random generation of rooms and tunnels

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::cmp;
+use rand::Rng;
 use tcod::colors::{self, Color};
 use tcod::console::*;
 
@@ -18,6 +19,10 @@ const COLOR_DARK_GROUND: Color = Color {
     g: 50,
     b: 150,
 };
+
+const ROOM_MAX_SIZE: i32 = 10;
+const ROOM_MIN_SIZE: i32 = 6;
+const MAX_ROOMS: i32 = 30;
 
 // A tile of the map, and its properties.
 #[derive(Clone, Copy, Debug)]
@@ -44,18 +49,56 @@ impl Tile {
 
 type Map = Vec<Vec<Tile>>;
 
-fn make_map() -> Map {
+fn make_map() -> (Map, (i32, i32)) {
     // Fill map with "unblocked" tiles.
     let mut map = vec![vec![Tile::wall(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
 
-    // Create two rooms
-    let room1 = Rect::new(20, 15, 10, 15);
-    let room2 = Rect::new(50, 15, 10, 15);
-    create_room(room1, &mut map);
-    create_room(room2, &mut map);
-    create_h_tunnel(25, 55, 23, &mut map);
+    let mut rooms = vec![];
+    let mut starting_position = (0, 0);
+    for _ in 0..MAX_ROOMS {
+        // random width and height
+        let w = rand::thread_rng().gen_range(ROOM_MIN_SIZE, ROOM_MAX_SIZE + 1);
+        let h = rand::thread_rng().gen_range(ROOM_MIN_SIZE, ROOM_MAX_SIZE + 1);
+        // random position without going out of the boundaries of the map
+        let x = rand::thread_rng().gen_range(0, MAP_WIDTH - w);
+        let y = rand::thread_rng().gen_range(0, MAP_HEIGHT - h);
 
-    map
+        let new_room = Rect::new(x, y, w, h);
+        // run through the other rooms and see if they intersect with this one
+        let failed = rooms.iter().any(|other_room|new_room.intersects_with(other_room));
+        if !failed {
+            // this means there are no intersections, so this room is valid
+            // "paint" it to the map's tiles
+            create_room(new_room, &mut map);
+
+            // center coordinates of the new room
+            let (new_x, new_y) = new_room.center();
+            if rooms.is_empty() {
+                // this is the first room, where the player starts
+                starting_position = (new_x, new_y);
+            } else {
+                // all rooms after the first
+                // connect it to the previous room with a tunnel
+                
+                // center coordinates of the previous room
+                let (prev_x, prev_y) = rooms[rooms.len() - 1].center();
+
+                // draw a coin (random bool value -- either true or false)
+                if rand::random() {
+                    // first move horizontally, then vertically
+                    create_h_tunnel(prev_x, new_x, prev_y, &mut map);
+                    create_v_tunnel(prev_y, new_y, new_x, &mut map);
+                } else {
+                    // first move vertically, then horizontally
+                    create_v_tunnel(prev_y, new_y, prev_x, &mut map);
+                    create_h_tunnel(prev_x, new_x, new_y, &mut map);
+                }
+            }
+            rooms.push(new_room);
+        }
+    }
+
+    (map, starting_position)
 }
 
 // Generic object definition: player, monster, items, etc.
@@ -123,8 +166,11 @@ fn main() {
     tcod::system::set_fps(LIMIT_FPS);
     let mut con = Offscreen::new(MAP_WIDTH, MAP_HEIGHT);
 
+    // Generate map (not currently drawn to screen).
+    let (map, (player_x, player_y)) = make_map();
+
     // Create object representing the player.
-    let player = Object::new(25, 23, '@', colors::WHITE);
+    let player = Object::new(player_x, player_y, '@', colors::WHITE);
 
     // Create object representing an NPC.
     let npc = Object::new(
@@ -136,9 +182,6 @@ fn main() {
 
     // List of objects
     let mut objects = [player, npc];
-
-    // Generate map (not currently drawn to screen).
-    let map = make_map();
 
     while !root.window_closed() {
         // clear the screen of previous frame data.
@@ -213,6 +256,19 @@ impl Rect {
             x2: x + w,
             y2: y + h,
         }
+    }
+
+    pub fn center(&self) -> (i32, i32) {
+        let center_x = (self.x1 + self.x2) / 2;
+        let center_y = (self.y1 + self.y2) / 2;
+        (center_x, center_y)
+    }
+
+    pub fn intersects_with(&self, other: &Rect) -> bool {
+        (self.x1 <= other.x2)
+            && (self.x2 >= other.x1)
+            && (self.y1 <= other.y2)
+            && (self.y2 >= other.y1)
     }
 }
 
