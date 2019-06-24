2a37a354f4744110fbeda6354a39d667db6d9ef8
ArcticPheenix <phoenix.ckelly@gmail.com>
2019-06-23 21:44:32 -0600

    Added room and hallway generation.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::cmp;
 use tcod::colors::{self, Color};
 use tcod::console::*;
 
@@ -45,11 +46,15 @@ type Map = Vec<Vec<Tile>>;
 
 fn make_map() -> Map {
     // Fill map with "unblocked" tiles.
-    let mut map = vec![vec![Tile::empty(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
+    let mut map = vec![vec![Tile::wall(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
 
-    // Place two pillars to test the map
-    map[30][22] = Tile::wall();
-    map[50][22] = Tile::wall();
+    // Create two rooms
+    let room1 = Rect::new(20, 15, 10, 15);
+    let room2 = Rect::new(50, 15, 10, 15);
+    create_room(room1, &mut map);
+    create_room(room2, &mut map);
+    create_h_tunnel(25, 55, 23, &mut map);
+
     map
 }
 
@@ -105,15 +110,7 @@ fn render_all(root: &mut Root, con: &mut Offscreen, objects: &[Object], map: &Ma
         object.draw(con);
     }
     // Blit the contents of the buffer to the root console.
-    blit(
-        con,
-        (0, 0),
-        (MAP_WIDTH, MAP_HEIGHT),
-        root,
-        (0, 0),
-        1.0,
-        1.0,
-    );
+    blit(con, (0, 0), (MAP_WIDTH, MAP_HEIGHT), root, (0, 0), 1.0, 1.0);
 }
 
 fn main() {
@@ -127,12 +124,7 @@ fn main() {
     let mut con = Offscreen::new(MAP_WIDTH, MAP_HEIGHT);
 
     // Create object representing the player.
-    let player = Object::new(
-        SCREEN_WIDTH / 2,
-        SCREEN_HEIGHT / 2,
-        '@',
-        tcod::colors::WHITE,
-    );
+    let player = Object::new(25, 23, '@', colors::WHITE);
 
     // Create object representing an NPC.
     let npc = Object::new(
@@ -203,4 +195,43 @@ fn handle_keys(root: &mut Root, player: &mut Object, map: &Map) -> bool {
         _ => {}
     }
     false
+}
+
+#[derive(Clone, Copy, Debug)]
+struct Rect {
+    x1: i32,
+    y1: i32,
+    x2: i32,
+    y2: i32,
+}
+
+impl Rect {
+    pub fn new(x: i32, y: i32, w: i32, h: i32) -> Self {
+        Rect {
+            x1: x,
+            y1: y,
+            x2: x + w,
+            y2: y + h,
+        }
+    }
+}
+
+fn create_room(room: Rect, map: &mut Map) {
+    for x in (room.x1 + 1)..room.x2 {
+        for y in (room.y1 + 1)..room.y2 {
+            map[x as usize][y as usize] = Tile::empty();
+        }
+    }
+}
+
+fn create_h_tunnel(x1: i32, x2: i32, y: i32, map: &mut Map) {
+    for x in cmp::min(x1, x2)..(cmp::max(x1, x2) + 1) {
+        map[x as usize][y as usize] = Tile::empty();
+    }
+}
+
+fn create_v_tunnel(y1: i32, y2: i32, x: i32, map: &mut Map) {
+    for y in cmp::min(y1, y2)..(cmp::max(y1, y2) + 1) {
+        map[x as usize][y as usize] = Tile::empty();
+    }
 }
